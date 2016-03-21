bb313fc2207d510ab6a2bf9bb1854320d56ba24a
Kevin NZENG ESSIMENGANE <knzeng-e@e3r1p1.42.fr>
2016-03-21 01:19:45 +0100

    Suppression du dernier segfault identifie -Fichier sans retour a la ligne

--- srcs/ft_create_map.c
+++ srcs/ft_create_map.c
@@ -6,7 +6,7 @@
 /*   By: knzeng-e <knzeng-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/12 05:16:04 by knzeng-e          #+#    #+#             */
-/*   Updated: 2016/03/18 18:02:11 by knzeng-e         ###   ########.fr       */
+/*   Updated: 2016/03/21 01:13:20 by knzeng-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ t_map	*ft_create_map(t_map *map, char *file)
 	ft_initialize("FILE", map, file);
 	if (file[0] != 0)
 	{
-		while (file[i++] != '\n')
-			;
+		while (file[i] && file[i] != '\n')
+			i++;
 		count = ++i;
-		while (file[i++] != '\n')
-			;
+		while (file[i] && file[i] != '\n')
+			i++;
 		map->nb_columns = i - count;
 	}
 	return (map);
